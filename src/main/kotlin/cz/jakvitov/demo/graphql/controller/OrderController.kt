2253ef7a0976a8e84ef7df961cd2fbabf23648a0
Jakub Vítovec <jakvitov@gmail.com>
2025-02-02 18:26:51 +0100

    Orders customer fetching

--- src/main/kotlin/cz/jakvitov/demo/graphql/controller/OrderController.kt
+++ src/main/kotlin/cz/jakvitov/demo/graphql/controller/OrderController.kt
@@ -1,10 +1,13 @@
 package cz.jakvitov.demo.graphql.controller
 
-import cz.jakvitov.demo.graphql.dto.Order
+import cz.jakvitov.demo.graphql.domain.Customers
+import cz.jakvitov.demo.graphql.domain.Orders
+import cz.jakvitov.demo.graphql.repository.CustomersRepository
 import cz.jakvitov.demo.graphql.repository.OrdersRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 
 @Controller
@@ -13,9 +16,17 @@ class OrderController {
     @Autowired
     lateinit var ordersRepository: OrdersRepository
 
+    @Autowired
+    lateinit var customersRepository: CustomersRepository
+
     @QueryMapping
-    fun latestOrders(@Argument page: Int, @Argument count: Int): List<Order> {
-        return ordersRepository.getLatestOrders(count, page * count).map { Order(it) }.toList()
+    fun latestOrders(@Argument page: Int, @Argument count: Int): List<Orders> {
+        return ordersRepository.getLatestOrders(count, page * count)
+    }
+
+    @SchemaMapping(typeName = "Order", field = "customer")
+    fun getCustomer(orders: Orders): Customers? {
+        return orders.customerId?.let { customersRepository.findById(it).orElse(null) }
     }
 
 }
--- src/main/kotlin/cz/jakvitov/demo/graphql/dto/Customer.kt
+++
@@ -1,6 +0,0 @@
-package cz.jakvitov.demo.graphql.dto
-
-data class Customer (val id: Long, val name: String?,
-    val contactName: String?, val address: String?,
-    val city: String?, val postalCode: String?, val country: String? ){
-}
